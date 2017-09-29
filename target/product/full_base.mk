4122a2af99e482601a021dbddf8cd3978c99f13a
AudioGod <audiogod.adi@gmail.com>
2017-09-29 09:07:53 +0530

    Build: Inherit Vendor SOKP
    Signed-off-by: AudioGod <audiogod.adi@gmail.com>

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -49,3 +49,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
 
 # Get everything else from the parent package
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_no_telephony.mk)
+
+# Inherit Vendor SOKP.
+$(call inherit-product, vendor/sokp/config/common.mk)

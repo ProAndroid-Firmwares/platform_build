7ed49b848f0deb31e2911a8b47e24fbf3705614b
AudioGod <audiogod.adi@gmail.com>
2017-10-16 14:56:25 +0530

    Build: Inherit Vendor Pro-Android
    Signed-off-by: AudioGod <audiogod.adi@gmail.com>

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -43,3 +43,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
 
 # Get everything else from the parent package
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_no_telephony.mk)
+
+# Inherit Vendor SOKP.
+$(call inherit-product, vendor/proandroid/config/common.mk)
